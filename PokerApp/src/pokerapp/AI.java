53e1fc73f0f228380fd7497608649ffa5dc47091
James <Kylar.gyre3@gmail.com>
2020-04-18 12:30:27 +1200

    Commented and edited

--- PokerApp/src/pokerapp/AI.java
+++ PokerApp/src/pokerapp/AI.java
@@ -6,18 +6,27 @@ package pokerapp;
  * aiType = 0 is simpleAI
  *
  * @author James Bird-Sycamore
- * Last Updated: 14/04/2020
+ * Last Updated: 18/04/2020
  */
 public class AI {
     
-    int aiType;
+    int aiType; // The ai used for the computer player.
     
+    /**
+     * Constructor: Creates the AI object
+     * 
+     * @param aiType The AI being used by the computer player. 
+     */
     public AI (int aiType) {
         this.aiType = aiType;
     }
     
+    /**
+     * Figures out which AI the computer is using, and then runs the AI.
+     * 
+     * @return The input for the computer's move. 
+     */
     public String runAI () {
-        String output = "";
         if (aiType == 0) {
             return simpleAI();
         } else {
@@ -26,7 +35,12 @@ public class AI {
         }
     }
     
-    public String simpleAI () {
+    /**
+     * Simple AI, always checks or calls.
+     * 
+     * @return The input for the computer's move. 
+     */
+    private String simpleAI () {
         String output;
         output = "c";
         return output;
